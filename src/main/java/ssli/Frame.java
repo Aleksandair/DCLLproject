7a99f47bada3042e435a4a0c8bf5643c8895fe7a
Aleksandair <aleksandair.mh@gmail.com>
2015-03-09 08:52:31 +0100

    Created main class.

---
+++ src/main/java/ssli/Frame.java
@@ -0,0 +1,7 @@
+package ssli;
+
+/**
+ * Created by Skander on 09/03/2015.
+ */
+public class Frame {
+}
---
+++ src/main/java/ssli/Game.java
@@ -0,0 +1,9 @@
+package ssli;
+
+/**
+ * Created by Skander on 09/03/2015.
+ */
+public class Game {
+
+    private final static int NB_FRAMES = 10;
+}
